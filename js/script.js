a50d4985ab9119378843c9f769a6e7b844af2442
Perex15 <149716430+Perex15@users.noreply.github.com>
2026-02-14 22:19:18 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const notificationAlert = document.querySelector(".notification-success-alert");
+
+function showNotification() {
+    notificationAlert.style.display = "inherit";
+}
+
+window.addEventListener("load", function() {
+    const loader = document.querySelector("#wifi-loader");
+    const container = document.querySelector(".header");
+    const heroSection = document.querySelector(".hero-section");
+    const productSection = document.querySelector(".product-section");
+    const aboutUsSection = document.querySelector(".about-us-section");
+    setTimeout(function() {
+        container.style.display = "inherit";
+        heroSection.style.display = "flex";
+        aboutUsSection.style.display = "grid";
+        productSection.style.display = "inherit";
+        loader.style.display = "none";
+    }, 6500); 
+});
+
+
+const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
+
+addToCartButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        showNotification();
+        setTimeout(function() {
+            notificationAlert.style.display = "none";
+        }, 3000);
+    });
+});
