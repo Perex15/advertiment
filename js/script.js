6fe9516634b534fdda77dcfa5341045aadd1b67d
Perex15 <149716430+Perex15@users.noreply.github.com>
2026-02-15 17:20:29 +0000

    Refactor modal handling and notification display

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const notificationAlert = document.querySelector(".notification-success-alert");
-
-function showNotification() {
-    notificationAlert.style.display = "inherit";
-}
+const closeBtn = document.querySelectorAll(".close-btn");
+const modal = document.querySelector("#popupLogin");
+const modalSignup = document.querySelector("#popupSignup");
+const shopBtn = document.querySelector(".shop-btn");
+const signupLink = document.querySelector("#signupLink");
+const loginLink = document.querySelector("#loginLink");
 
 window.addEventListener("load", function() {
     const loader = document.querySelector("#wifi-loader");
@@ -16,7 +17,15 @@ window.addEventListener("load", function() {
         aboutUsSection.style.display = "grid";
         productSection.style.display = "inherit";
         loader.style.display = "none";
-    }, 6500); 
+    }, 1500); 
+
+    // if (modalSignup.classList.contains("active") === true) {
+        
+    // } else {
+    //     setTimeout(function() {
+    //     modal.classList.add("active");
+    //     }, 10000);
+    // }
 });
 
 
@@ -24,12 +33,32 @@ const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
 
 addToCartButtons.forEach(button => {
     button.addEventListener("click", function() {
-        showNotification();
-        setTimeout(function() {
-            notificationAlert.style.display = "none";
-        }, 3000);
+        modal.classList.add("active");
+        // window.alert("Product added to cart! \n Project is still in development, so the cart functionality is not yet implemented.");
+        // showNotification();
+        // setTimeout(function() {
+        //     notificationAlert.style.display = "none";
+        // }, 3000);
     });
-
 });
 
+shopBtn.addEventListener("click", function() {
+    modal.classList.add("active");
+});
 
+signupLink.addEventListener("click", function() {
+    modal.classList.remove("active");
+    modalSignup.classList.add("active");
+});
+
+loginLink.addEventListener("click", function() {
+    modalSignup.classList.remove("active");
+    modal.classList.add("active");
+});
+
+closeBtn.forEach(btn => {
+    btn.addEventListener("click", function() {
+        modal.classList.remove("active");
+        modalSignup.classList.remove("active");
+    });
+});
